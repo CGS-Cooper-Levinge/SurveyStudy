47629878898e3bb2df866ec236920ed9b4a98e4e
Cooper Levinge <Cooper@Coopers-MacBook-Pro.local>
2017-10-24 11:58:01 +1100

    The second commit

---
+++ Survey study/EmailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EmailViewController.swift
+//  Survey study
+//
+//  Created by Cooper Levinge on 15/10/17.
+//  Copyright © 2017 Cooper Levinge. All rights reserved.
+//
+
+import Foundation
